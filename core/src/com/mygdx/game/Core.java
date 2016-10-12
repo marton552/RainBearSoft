26479f92c1bac22163808a2cd9605447004170ed
istvan12345678 <tothistvan12345678@gmail.com>
2016-10-12 09:37:52 +0200

    asd

--- core/src/com/mygdx/game/Core.java
+++ core/src/com/mygdx/game/Core.java
@@ -2,6 +2,12 @@ package com.mygdx.game;
 
 public class Core {
     public void generalas(){
+        for (int i = 0; i<5; i++){
+
+        }
+        for (int i = 0; i<4; i++){
+
+        }
 
     }
 }

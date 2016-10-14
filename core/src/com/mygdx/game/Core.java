c8e3e89763446c34d9c787fd14b2f5bae4d8e0f4
istvan12345678 <tothistvan12345678@gmail.com>
2016-10-14 13:21:09 +0200

    2016.10.14 13:20

--- core/src/com/mygdx/game/Core.java
+++ core/src/com/mygdx/game/Core.java
@@ -2,6 +2,13 @@ package com.mygdx.game;
 
 public class Core {
     public void generalas(){
+        int[] sorozatszamok = {};
+        int[] sorozatmuveletek = {};
+        Random random = new Random();
+        int utolsoszam = 0;
+        int[] kepletszamok = {random.nextInt(8) + 1, random.nextInt(8) + 1, random.nextInt(8) + 1};
+        int[] muveletek = {random.nextInt(2) + 1, random.nextInt(2) + 1, random.nextInt(2) + 1};
+
         for (int i = 0; i<5; i++){
 
         }
--- core/src/com/mygdx/game/MenuStage.java
+++ core/src/com/mygdx/game/MenuStage.java
@@ -54,22 +54,6 @@ public class MenuStage extends MyStage {
         addActor(animation);
         addActor(cim);
 
-        int[] sorozatszamok = {};
-        int[] sorozatmuveletek = {};
-        Random random = new Random();
-        int utolsoszam = 0;
-        int[] kepletszamok = {random.nextInt(8) + 1, random.nextInt(8) + 1, random.nextInt(8) + 1};
-        int[] muveletek = {random.nextInt(2) + 1, random.nextInt(2) + 1, random.nextInt(2) + 1};
-
-        for(int i = 0; i < 4; i++){
-
-        }
-
-        for(int i = 0; i < sorozat.length; i++){
-            System.out.print(sorozat[i]+", ");
-        }
-    }
-
 
     @Override
     public void act(float delta) {
